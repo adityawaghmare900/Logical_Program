c0fb170d402b5917542c5ce4cb33a24b6da58735
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-03 20:48:29 +0530

    [aditya]: Fabbonacci Series

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LogicalProgam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=0; int b=1;
+            Console.Write(a+" "+ b+" "); 
+            for(int i=0;i<10;i++)
+            {
+                int c = a + b;
+                Console.Write(c+" ");
+                a = b;
+                b=c;
+            }
+        }
+    }
+}
